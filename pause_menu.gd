16a5b8f05b470b1a384bc3b961d5b9585afd54bf
NeveroyatnoOpasen <glebplahotny@gmail.com>
2025-11-08 10:30:53 +0500

    menu prime
    menu fixxx

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,10 +1,20 @@
 extends Control
+func _ready():
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	$AnimationPlayer.process_mode = Node.PROCESS_MODE_ALWAYS
+	$AnimationPlayer.play("RESET")
+	hide()
+
+func pause():
+	show() # показываем меню
+	$AnimationPlayer.play("blur_menu") # проигрываем появление
+	get_tree().paused = true # ставим игру на паузу
 
 func resume():
-	get_tree().paused = false
-	$AnimationPlayer.play_backwards("blur_menu")
-func pause():
-	get_tree().paused = true
+	get_tree().paused = false # снимаем паузу
+	$AnimationPlayer.play_backwards("blur_menu") # проигрываем исчезновение
+	hide() # прячем меню (или дождись конца анимации, если хочешь красиво)
+	
 	$AnimationPlayer.play_backwards("blur_menu")
 func escape():
 	if Input.is_action_just_pressed("esc") and !get_tree().paused:
@@ -19,5 +29,9 @@ func _on_resume_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
-func _process(delta):
-	escape()
+func _input(event):
+	if event.is_action_pressed("esc"):
+		if get_tree().paused:
+			resume()
+		else:
+			pause()
